191f66b5d5d66398cb4326375b42bcd6e16d5e74
Prerit02 <62843230+Prerit02@users.noreply.github.com>
2020-06-16 16:56:02 +0530

    Update BubbleSort.py

--- Sorting Algos/BubbleSort.py
+++ Sorting Algos/BubbleSort.py
@@ -1,3 +1,13 @@
+Bubble Sort:-
+Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that repeatedly steps through the list, compares 
+adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted.
+
+Some Notes:-
+Worst and Average Case Time Complexity: O(n*n). Worst case occurs when array is reverse sorted.
+Best Case Time Complexity: O(n). Best case occurs when array is already sorted.
+Auxiliary Space: O(1)
+Boundary Cases: Bubble sort takes minimum time (Order of n) when elements are already sorted.
+
 # Bubble Sort Algorithm
 import random
 n = int(input("Enter the length of the list: "))
